bc078fe51a0ff6856417aa15f9974e79a5e14821
Jacob Foley <jacob.j.foley@gmail.com>
2020-02-11 18:09:46 +1100

    Configure webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,27 @@
 const path = require("path");
 
 module.exports = {
-  entry: "./src/index.js",
+  mode: "development",
+  entry: "./src/index.ts",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"]
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: path.resolve("static"),
+    publicPath: path.resolve("static")
   }
 };
