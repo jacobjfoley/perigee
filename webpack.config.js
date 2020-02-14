45b5411fa93b4d6733ab0baf2ee0500ad0f28bbc
Jacob Foley <jacob.j.foley@gmail.com>
2020-02-14 13:29:29 +1100

    Improve asset bundling

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,13 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
   mode: "development",
   entry: "./src/index.tsx",
   output: {
-    filename: "main.js",
+    filename: "main-[contentHash].js",
     path: path.resolve(__dirname, "dist")
   },
   module: {
@@ -13,14 +16,22 @@ module.exports = {
         test: /\.tsx?$/,
         use: "ts-loader",
         exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
       }
     ]
   },
   resolve: {
     extensions: [".tsx", ".ts", ".js"]
   },
-  devtool: "inline-source-map",
-  devServer: {
-    contentBase: path.join(__dirname, "static")
-  }
+  plugins: [
+    new CleanWebpackPlugin(),
+    new CopyWebpackPlugin([{ from: path.join(__dirname, "static") }]),
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "src", "index.html")
+    })
+  ],
+  devtool: "inline-source-map"
 };
