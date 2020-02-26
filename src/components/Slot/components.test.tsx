5f78d040b273e27615f1cdd6fbf8e01796361373
Jacob Foley <jacob.j.foley@gmail.com>
2020-02-26 12:48:36 +1100

    Add new tests

--- src/components/Slot/components.test.tsx
+++ src/components/Slot/components.test.tsx
@@ -1,9 +1,23 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 
 import Slot from "./components";
 import { createDrone } from "../../actions";
 
+type CreateDrone = typeof createDrone;
+
+interface MockedCreateDrone extends CreateDrone {
+  mock: {
+    calls: {
+      length: number;
+    };
+  };
+}
+
 test("it renders", () => {
-  render(<Slot createDrone={createDrone} />);
+  const mock: MockedCreateDrone = Object.assign(jest.fn(), createDrone);
+  render(<Slot createDrone={mock} />);
+  const button = screen.getByText(/Create/i);
+  fireEvent.click(button);
+  expect(mock.mock.calls.length).toBe(1);
 });
